d0879a8a4b60de7429b66e188e8ea85327626ec2
CarlosAdrianJ <aslak71@hotmail.com>
2025-08-27 09:24:29 -0300

    agregue foto hotel para ejemplo

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,7 +3,12 @@ function Home() {
 
     return (    
         <>
-        <h1>Bienvenidos al Hotel</h1>
+        <h1 className="text-dark">Bienvenidos al Hotel</h1> 
+        <img 
+        src="https://firebasestorage.googleapis.com/v0/b/homestate-web.firebasestorage.app/o/hotel_carlos.PNG?alt=media&token=74053bf5-5686-4147-ac37-2552c5438fd8" 
+        alt="Hotel de lujo" 
+        style={{ width: "800px", borderRadius: "10px" }} 
+      />
         <p>Hotel de Lujo ubicado en zona centrica</p>
         </>
     )
