2496e3ff878945d3eacf3225cb9d3d503e8860f8
CarlosAdrianJ <aslak71@hotmail.com>
2025-09-01 15:58:27 -0300

    agregue una estructura a la home y foto

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,16 +1,40 @@
-
+import starlightless from '../../images/starlightless.PNG';
+import { Container, Row, Col, Image } from "react-bootstrap";
 function Home() {
 
-    return (    
-        <>
-        <h1 className="text-dark">Bienvenidos al Hotel</h1> 
-        <img 
-        src="https://firebasestorage.googleapis.com/v0/b/homestate-web.firebasestorage.app/o/hotel_carlos.PNG?alt=media&token=74053bf5-5686-4147-ac37-2552c5438fd8" 
+    return (   
+         <div>
+        <div className='covertor-imagen-fondo'>
+        
+        <Image
+        src ={starlightless}
         alt="Hotel de lujo" 
-        style={{ width: "800px", borderRadius: "10px" }} 
-      />
+        fluid
+        className='estilo-imagen-fondo'
+        />
+        
         <p>Hotel de Lujo ubicado en zona centrica</p>
-        </>
+        </div>
+           <Container> 
+            <Row>
+
+           
+            <Col> 
+            <p>En pleno corazón de Rosario, este hotel 5 estrellas combina elegancia y modernidad para ofrecer una experiencia única. Su diseño sofisticado y ambientes amplios brindan el máximo confort tanto para viajes de negocios como para escapadas de placer. </p>
+            <p>A pocas cuadras del Monumento a la Bandera y de la peatonal Córdoba, la ubicación es ideal para quienes buscan comodidad y cercanía a los principales atractivos turísticos y comerciales de la ciudad. Cada detalle está pensado para garantizar bienestar y exclusividad.</p>
+            <p>El hotel cuenta con habitaciones de lujo, spa, piscina y servicio de primer nivel. Además, se encuentra muy cerca de la vibrante avenida Pellegrini, reconocida por su variada oferta gastronómica, lo que permite disfrutar de la mejor cocina local a pasos del alojamiento.</p>
+            </Col>
+             <Col>
+            
+            </Col>
+            
+            </Row>
+
+
+
+
+           </Container>
+        </div>
     )
 }
 
